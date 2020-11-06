4117e3f05ee9cfb821ddcee38d4d19cfaa693752
Dmitry Safonov <dima@arista.com>
2020-11-06 14:34:09 +0000

    mm/mmap: Make vm_special_mapping::mremap return void
    Previously .mremap() callback needed (int) return to provide way to
    restrict resizing of a special mapping. Now it's restricted by
    providing .may_split = special_mapping_split.
    
    Removing (int) return simplifies further changes to
    special_mapping_mremap() as it won't need save ret code from the
    callback. Also, it removes needless `return 0` from callbacks.
    
    Signed-off-by: Dmitry Safonov <dima@arista.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -772,7 +772,7 @@ struct vm_special_mapping {
 				struct vm_area_struct *vma,
 				struct vm_fault *vmf);
 
-	int (*mremap)(const struct vm_special_mapping *sm,
+	void (*mremap)(const struct vm_special_mapping *sm,
 		     struct vm_area_struct *new_vma);
 };
 
