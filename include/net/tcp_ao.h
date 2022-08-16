3ccd969a74fc256c387028c6323a2cf9f68c873d
Dmitry Safonov <dima@arista.com>
2022-08-16 18:11:20 +0100

    net/tcp: Ignore specific ICMPs for TCP-AO connections
    Similarly to IPsec, RFC5925 prescribes:
      ">> A TCP-AO implementation MUST default to ignore incoming ICMPv4
      messages of Type 3 (destination unreachable), Codes 2-4 (protocol
      unreachable, port unreachable, and fragmentation needed -- ’hard
      errors’), and ICMPv6 Type 1 (destination unreachable), Code 1
      (administratively prohibited) and Code 4 (port unreachable) intended
      for connections in synchronized states (ESTABLISHED, FIN-WAIT-1, FIN-
      WAIT-2, CLOSE-WAIT, CLOSING, LAST-ACK, TIME-WAIT) that match MKTs."
    
    A selftest (later in patch series) verifies that this attack is not
    possible in this TCP-AO implementation.
    
    Co-developed-by: Francesco Ruggeri <fruggeri@arista.com>
    Signed-off-by: Francesco Ruggeri <fruggeri@arista.com>
    Co-developed-by: Salam Noureddine <noureddine@arista.com>
    Signed-off-by: Salam Noureddine <noureddine@arista.com>
    Signed-off-by: Dmitry Safonov <dima@arista.com>

--- include/net/tcp_ao.h
+++ include/net/tcp_ao.h
@@ -25,6 +25,7 @@ struct tcp_ao_counters {
 	atomic64_t	pkt_bad;
 	atomic64_t	key_not_found;
 	atomic64_t	ao_required;
+	atomic64_t	dropped_icmp;
 };
 
 struct tcp_ao_key {
@@ -94,7 +95,8 @@ struct tcp_ao_info {
 	struct tcp_ao_key	*rnext_key;
 	struct tcp_ao_counters	counters;
 	u32			ao_required	:1,
-				__unused	:31;
+				accept_icmps	:1,
+				__unused	:30;
 	__be32			lisn;
 	__be32			risn;
 	u32			snd_sne;
@@ -174,6 +176,7 @@ int tcp_ao_calc_traffic_key(struct tcp_ao_key *mkt, u8 *key, void *ctx,
 void tcp_ao_destroy_sock(struct sock *sk, bool twsk);
 u32 tcp_ao_compute_sne(u32 sne, u32 seq, u32 new_seq);
 void tcp_ao_time_wait(struct tcp_timewait_sock *tcptw, struct tcp_sock *tp);
+bool tcp_ao_ignore_icmp(struct sock *sk, int type, int code);
 enum skb_drop_reason tcp_inbound_ao_hash(struct sock *sk,
 			const struct sk_buff *skb, unsigned short int family,
 			const struct request_sock *req,
@@ -246,6 +249,11 @@ static inline void tcp_ao_syncookie(struct sock *sk, const struct sk_buff *skb,
 		      struct tcp_request_sock *treq,
 		      unsigned short int family)
 {
+}
+
+static inline bool tcp_ao_ignore_icmp(struct sock *sk, int type, int code)
+{
+	return false;
 }
 
 static inline enum skb_drop_reason tcp_inbound_ao_hash(struct sock *sk,
